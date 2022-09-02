24330dade158ba7cacf4933173609e6d995f68b7
Pin <pino.chang@tpisoftware.com>
2022-09-02 16:06:28 +0800

    feat: create spring boot project

---
+++ src/test/java/com/pino/mailsample/MailSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pino.mailsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
